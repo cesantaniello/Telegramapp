e69013dd996084371c6a1dfdea40f09610292e43
Administrator <admin@example.com>
2021-11-25 11:38:56 +0100

    2. Agrega routers: git, post y delete

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-app.use('/', function (req, res) {
-    res.send('Hola');
+router.get('/message', function (req, res) {
+    res.send('Lista de mensajes');
 });
+
+router.post('/message', function (req, res) {
+    res.send('Mensaje agregado');
+});
+
+router.delete('/message', function (req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Mensaje '+ req.body.text +' borrado');
+});
+
+// app.use('/', function (req, res) {
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('La aplicación está escuchando en http://localhost:3000');
