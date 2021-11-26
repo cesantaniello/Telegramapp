e867f9610338eceff7b3bf79c3f2f35918dc6e44
Administrator <admin@example.com>
2021-11-26 16:44:53 +0100

    11. Establece conexion con Base de Datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,32 @@
 const db = require('mongoose');
+const Model = require('./model')
+
+const uri = 'mongodb+srv://admin:admin@cluster0.ni2ao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+db.Promise = global.Promise;
+
+db.connect(uri, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Conectado a MongoDB');
+    })
+    .catch(err => {
+        console.error('No se ha podido conectar a la BB. DD.');
+});
+
 
 function addMessage(message) {
-    list.push(message);
+    // list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    // return list;
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
