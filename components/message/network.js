f2df7452d675f129cfb65be55d96edc23b4691ee
Administrator <admin@example.com>
2021-11-26 18:48:48 +0100

    12. Actualiza la Base de Datos

--- components/message/network.js
+++ components/message/network.js
@@ -24,4 +24,14 @@ router.post('/', function (req, res) {
         });
 });
 
+router.patch('/:id', function (req, res){
+    controller.updateMessage(req.params.id, req.body.message)
+        .then((data) => {
+            response.success(req, res, data, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Error interno', 500, e);
+        });
+})
+
 module.exports = router;
